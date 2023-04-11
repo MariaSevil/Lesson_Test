c99de6bda66c7820e9b4987d37fb463679fdea04
Maria Sevil <sevil1505@gmail.com>
2023-04-12 00:13:18 +0300

    added to the Task Lesson_1

--- Lesson_1/Program.cs
+++ Lesson_1/Program.cs
@@ -2,9 +2,9 @@
 //Написать программу, которая из имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 
 
-string[] originalArray = {"Hello", "2", "world", ":-)"};
-string[] newArray = new string[originalArray.Length]; 
-int count = 0; 
+string[] originalArray = { "Hello", "2", "world", ":-)" };
+string[] newArray = new string[originalArray.Length];
+int count = 0;
 
 for (int i = 0; i < originalArray.Length; i++)
 {
@@ -15,5 +15,11 @@ for (int i = 0; i < originalArray.Length; i++)
     }
 }
 
+Array.Resize(ref newArray, count); // изменить размер нового массива до нужной длины
+
+for (int i = 0; i < newArray.Length; i++)
+{
+    Console.WriteLine(newArray[i]); //вывести каждую строку в новый массив
+}
 
 
